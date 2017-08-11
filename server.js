d72c07f1936127e1f7e3981d175205b1ca541eb6
JimJimJam <30717772+JimJimJam@users.noreply.github.com>
2017-08-11 13:33:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/article-1', function(req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-1.html')); 
-});
+
 
 app.get('/ui/article-1', function(req, res) {
    res.send("welcome article 1"); 

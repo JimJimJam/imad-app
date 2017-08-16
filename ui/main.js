34062eb7f319b3a38b4485f018651536fd044121
JimJimJam <30717772+JimJimJam@users.noreply.github.com>
2017-08-16 13:40:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 var element = document.getElementById('here');
 
 element.innerHTML = "<h3>Replaced Original data</h3>";
+
+var img = document.getElementById('image');
+
+img.onclick = function() {
+    img.style.marginLeft = '10px';
+    
+};

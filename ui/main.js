59a21c625c7523f8e05b3015e8bfebd1a981cb3e
JimJimJam <30717772+JimJimJam@users.noreply.github.com>
2017-08-19 13:59:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,5 +25,5 @@ img.onclick = function(){
 
 but.onclick = function(){
   bcounter = bcounter + 1;
-  clickdisplay.innerHTML(bcounter.toString());
+  clickdisplay.innerHTML = bcounter.toString();
 };

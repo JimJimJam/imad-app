1c17d43f446d78bc7c119f1a5e1656c979fa5182
JimJimJam <30717772+JimJimJam@users.noreply.github.com>
2017-08-19 14:05:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,5 +25,5 @@ img.onclick = function(){
 
 but.onclick = function(){
   bcounter = bcounter + 1;
-  clickdisplay.innerHTML = "The button has been clicked" + bcounter.toString() + "times";
+  clickdisplay.innerHTML = "The button has been clicked" + " " + bcounter.toString() + " " + "times";
 };

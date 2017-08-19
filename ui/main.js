6f6c5c9c178f9f8b72526aa53d6d825e6fe7e800
JimJimJam <30717772+JimJimJam@users.noreply.github.com>
2017-08-19 17:18:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,6 +14,22 @@ var clickdisplay = document.getElementById("times");
 
 var margin =0;
 
+//create  request object 
+
+var request = new XMLHttpRequest();
+     request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE)
+       { //take some action
+       }
+            if(request.status === 200)
+             {
+                 var count = request.responseText;
+                 var span = document.getElementById('The counter variable has the value:' + count);
+             }
+             
+    
+    };
+    
  function moveRight() {
     margin = margin + 10;
     img.style.marginLeft = margin + "px";
@@ -27,3 +43,8 @@ but.onclick = function(){
   bcounter = bcounter + 1;
   clickdisplay.innerHTML = "The button has been clicked" + " " + bcounter.toString() + " " + "times";
 };
+
+//invoke the request
+
+request.open(GET,"http://lincet123.imad.hasura-app.io/counter");
+request.send(null);
